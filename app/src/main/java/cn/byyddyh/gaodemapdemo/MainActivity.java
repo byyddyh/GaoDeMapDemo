276c0a51d3cd2d437388fc288d634724352989dd
byyddyh <byyddyh@gmail.com>
2023-01-31 15:26:39 +0800

    Initial commit

---
+++ app/src/main/java/cn/byyddyh/gaodemapdemo/MainActivity.java
@@ -0,0 +1,219 @@
+package cn.byyddyh.gaodemapdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.MapsInitializer;
+import com.amap.api.services.core.ServiceSettings;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements AMapLocationListener {
+
+    //请求权限码
+    private static final int REQUEST_PERMISSIONS = 9527;
+
+    //内容
+//    private MapView mapView;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Context context = this;
+        //定位隐私政策同意
+        AMapLocationClient.updatePrivacyShow(context,true,true);
+        AMapLocationClient.updatePrivacyAgree(context,true);
+        //地图隐私政策同意
+        MapsInitializer.updatePrivacyShow(context,true,true);
+        MapsInitializer.updatePrivacyAgree(context,true);
+        //搜索隐私政策同意
+        ServiceSettings.updatePrivacyShow(context,true,true);
+        ServiceSettings.updatePrivacyAgree(context,true);
+
+//        mapView = findViewById(R.id.map_view);
+//        mapView.onCreate(savedInstanceState);
+        textView = findViewById(R.id.tv_content);
+
+        // 初始化定位
+        initLocation();
+
+        // 检查Android版本
+        checkingAndroidVersion();
+    }
+
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        //销毁定位客户端，同时销毁本地定位服务。
+//        mLocationClient.onDestroy();
+//        // 在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+//        mapView.onDestroy();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+//        mapView.onResume();
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+//        mapView.onPause();
+//    }
+//
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+//        mapView.onSaveInstanceState(outState);
+//    }
+
+    /**
+     * 检查Android版本
+     */
+    private void checkingAndroidVersion() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            //Android6.0及以上先获取权限再定位
+            requestPermission();
+        }else {
+            //Android6.0以下直接定位
+            mLocationClient.startLocation();
+        }
+    }
+
+
+    /**
+     * 动态请求权限
+     */
+    @AfterPermissionGranted(REQUEST_PERMISSIONS)
+    private void requestPermission() {
+        String[] permissions = {
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.READ_PHONE_STATE,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+
+        if (EasyPermissions.hasPermissions(this, permissions)) {
+            //true 有权限 开始定位
+            showMsg("已获得权限，可以定位啦！");
+
+            mLocationClient.startLocation();
+        } else {
+            //false 无权限
+            EasyPermissions.requestPermissions(this, "需要权限", REQUEST_PERMISSIONS, permissions);
+        }
+    }
+
+    /**
+     * 请求权限结果
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //设置权限请求结果
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    /**
+     * Toast提示
+     * @param msg 提示内容
+     */
+    private void showMsg(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
+
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
+
+    /**
+     * 初始化定位
+     */
+    private void initLocation() {
+        //初始化定位
+        try {
+            mLocationClient = new AMapLocationClient(getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (mLocationClient != null) {
+            //设置定位回调监听
+            mLocationClient.setLocationListener(this);
+            //初始化AMapLocationClientOption对象
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //获取最近3s内精度最高的一次定位结果：
+            //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+            mLocationOption.setOnceLocationLatest(true);
+            //设置是否返回地址信息（默认返回地址信息）
+            mLocationOption.setNeedAddress(true);
+            //设置定位请求超时时间，单位是毫秒，默认30000毫秒，建议超时时间不要低于8000毫秒。
+            mLocationOption.setHttpTimeOut(20000);
+            //关闭缓存机制，高精度定位会产生缓存。
+            mLocationOption.setLocationCacheEnable(false);
+            //给定位客户端对象设置定位参数
+            mLocationClient.setLocationOption(mLocationOption);
+        }
+    }
+
+    /**
+     * 接收异步返回的定位结果
+     *
+     * @param aMapLocation
+     */
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                // 地址
+                String address = aMapLocation.getAddress();
+                // 获取纬度
+                double latitude = aMapLocation.getLatitude();
+                // 获取经度
+                double longitude = aMapLocation.getLongitude();
+                StringBuilder buffer = new StringBuilder();
+                buffer.append("纬度: ").append(latitude).append("\n");
+                buffer.append("经度: ").append(longitude).append("\n");
+                buffer.append("地址: ").append(address).append("\n");
+
+//                Log.d("MainActivity", buffer.toString());
+                textView.setText(address == null ? "无地址": buffer);
+                showMsg(address);
+
+                // 停止定位后，本地定位服务并不会被销毁
+                mLocationClient.stopLocation();
+            } else {
+                //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError", "location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+            }
+        }
+    }
+
+}
